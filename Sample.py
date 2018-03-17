565f8a8e6ccb2bcf9fd66070468e9125f62fb8ec
Karolishp <31632511+KaroliShp@users.noreply.github.com>
2018-03-17 15:16:38 +0000

    Add files via upload

--- Sample.py
+++ Sample.py
@@ -10,20 +10,77 @@ import Leap, sys, thread, time
 from Leap import CircleGesture, KeyTapGesture, ScreenTapGesture, SwipeGesture
 import cv2
 import time
+import json
+import math
+
+def angle(v1,v2):
+    top = (v1[0] * v2[0]) + (v1[1] * v2[1]) + (v1[2] * v2[2])
+    bottom = math.sqrt((v1[0]*v1[0]) + (v1[1]*v1[1]) + (v1[2]*v1[2])) * math.sqrt((v2[0]*v2[0]) + (v2[1]*v2[1]) + (v2[2]*v2[2]))
+    angle = math.acos(top/bottom)
+    return angle
 
 def main():
     controller = Leap.Controller()
 
     time.sleep(2)
+    count = 0
+    json_formatted_data_none = {}
+    final_data = {}
+
+    print("None")
 
     while controller.is_connected:
         frame = controller.frame()
-        hands = frame.hands
-        pointables = frame.pointables
-        fingers = frame.fingers
-        tools = frame.tools
-        print(fingers[0].bone(1).direction)
-        time.sleep(2)
+        for hand in frame.hands:
+            finger_dict = {}
+            for finger in hand.fingers:
+                if finger.type == 0:
+                    continue
+                angle12 = angle(finger.bone(1).direction, finger.bone(2).direction)
+                angle23 = angle(finger.bone(2).direction, finger.bone(3).direction)
+                angle3palm = angle(finger.bone(3).direction, hand.palm_normal)
+                finger_dict[finger.type] = {"MPA": angle12, "PIA": angle23, "IPA": angle3palm}
+            json_formatted_data_none[str(count)] = finger_dict
+        count = count + 1
+        time.sleep(1)
+        print(count)
+        if count == 5:
+            break
+
+    final_data["none"] = json_formatted_data_none
+
+    print("1")
+
+    count = 0
+
+    json_formatted_data_1 = {}
+
+    while controller.is_connected:
+        frame = controller.frame()
+        for hand in frame.hands:
+            finger_dict = {}
+            for finger in hand.fingers:
+                if finger.type == 0:
+                    continue
+                angle12 = angle(finger.bone(1).direction, finger.bone(2).direction)
+                angle23 = angle(finger.bone(2).direction, finger.bone(3).direction)
+                angle3palm = angle(finger.bone(3).direction, hand.palm_normal)
+                finger_dict[finger.type] = {"MPA": angle12, "PIA": angle23, "IPA": angle3palm}
+            json_formatted_data_1[str(count)] = finger_dict
+        count = count + 1
+        time.sleep(1)
+        print(count)
+        if count == 5:
+            break
+
+    final_data["1"] = json_formatted_data_1
+
+    final_data = json.dumps(final_data)
+
+    text_file = open("output_file.json", "r+")
+    text_file.write(final_data)
+    text_file.close()
+
 
     # Keep this process running until Enter is pressed
     print "Press Enter to quit..."
